496ab4ddbb1bc6c8ff0aa8dc5c3ab476aa8cdc84
Gustavo Tejerina Santos <39309543+gustavoprimitive@users.noreply.github.com>
2025-10-15 17:01:46 +0200

    Update win_activity_emulation.py

--- win_activity_emulation.py
+++ win_activity_emulation.py
@@ -5,23 +5,22 @@ import time
 import pydirectinput
 from datetime import datetime
 
-opciones = ["up", "down", "left", "right", "space", "tab", "esc", "home", "end", "pageup", "pagedown"];
+actions = ["up", "down", "left", "right", "space", "tab", "esc", "home", "end", "pageup", "pagedown"];
 
 print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
-cont = 0
+counter = 0
 while 1 == 1:
-    cont += 1
+    counter += 1
     x = random.randrange(1, 1000)
     y = random.randrange(1, 1000)
     win32api.SetCursorPos((x, y))
     #win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
     #win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-    if cont == 10:
+    if counter == 10:
         #pydirectinput.click(button='right')
-        #pydirectinput.press('esc')	
-        opcion = random.choice(opciones)
+        #pydirectinput.press('esc')
+        opcion = random.choice(action)
         pydirectinput.press(opcion)
-        #print(cont, " ", opcion)
-        cont = 0
+        counter = 0
     time.sleep(random.randrange(1, 10))
